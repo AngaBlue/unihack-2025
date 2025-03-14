0cb04509ddcca8edb254e8b24cc24b35680dc8f9
AngaBlue <contact@anga.blue>
2025-03-15 00:06:28 +1100

    fix: client component

--- app/client.tsx
+++ app/client.tsx
@@ -3,7 +3,11 @@
 import { hydrate } from '@/util/hydrated';
 import { ToastType, createToast } from '@/util/toasts';
 import { useEffect } from 'react';
-import ToastWrapper from './components/ToastWrapper';
+import Terrarium from './Terrarium';
+import TerrariumTwo from './TerrariumTwo';
+import { ThreeProvider } from './ThreeContext';
+import ThreeEnvironment from './ThreeEnvironment';
+import * as THREE from 'three';
 
 export default function Client() {
 	useEffect(() => {
@@ -15,5 +19,15 @@ export default function Client() {
 		});
 	}, []);
 
-	return <ToastWrapper />;
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 45, 30000);
+
+	return (
+		<ThreeProvider scene={scene} camera={camera}>
+			<ThreeEnvironment>
+				<Terrarium />
+				<TerrariumTwo />
+			</ThreeEnvironment>
+		</ThreeProvider>
+	);
 }
