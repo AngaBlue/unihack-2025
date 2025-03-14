b24a556c8b42206de07902f923f52ba99097c6b9
Luca Lowndes <104835046+LucaLow@users.noreply.github.com>
2025-03-14 20:09:28 +1100

    test

--- app/GetSkills/route.ts
+++ app/GetSkills/route.ts
@@ -3,5 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
     // Your API logic here
     console.log(process.env.apiKey);
+    console.log("yay")
     return NextResponse.json({ message: 'Hello from Next.js!' });
 }
