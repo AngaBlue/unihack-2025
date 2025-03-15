46a684dbca788ead79c45cea763c62a07bd0b7cd
CaMo111 <109951019+CaMo111@users.noreply.github.com>
2025-03-15 13:24:38 +1100

    Add init function which initialises scene with planet. Lighting is bad.

--- app/Terrarium.tsx
+++ app/Terrarium.tsx
@@ -7,25 +7,28 @@ const Terrarium = () => {
 	const { scene, camera } = useThree();
 
 	useEffect(() => {
-		const geometry = new THREE.BoxGeometry(150, 150, 150);
-		const edges = new THREE.EdgesGeometry(geometry);
+		const geometry = new THREE.SphereGeometry( 15, 32, 16 ); 
+		const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+		const sphere = new THREE.Mesh( geometry, material ); scene.add( sphere );
+		// const geometry = new THREE.BoxGeometry(150, 150, 150);
+		// const edges = new THREE.EdgesGeometry(geometry);
 
-		const material = new THREE.LineBasicMaterial({
-			color: 0xffffff,
-			transparent: true,
-			linejoin: 'bevel',
-			opacity: 0.8
-		});
+		// const material = new THREE.LineBasicMaterial({
+		// 	color: 0xffffff,
+		// 	transparent: true,
+		// 	linejoin: 'bevel',
+		// 	opacity: 0.8
+		// });
 
-		const line = new THREE.LineSegments(edges, material);
+		// const line = new THREE.LineSegments(edges, material);
 
 		scene.add(line);
 
-		line.position.set(0, 0, -5);
+		// line.position.set(0, 0, -5);
 
 		// Animation loop to update the scene
 		const animate = () => {
-			line.rotation.y += 0.0005;
+			// line.rotation.y += 0.0005;
 			requestAnimationFrame(animate);
 		};
 
--- app/client.tsx
+++ app/client.tsx
@@ -2,16 +2,18 @@
 
 import { hydrate } from '@/util/hydrated';
 import { ToastType, createToast } from '@/util/toasts';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
 import Terrarium from './Terrarium';
 import TerrariumTwo from './TerrariumTwo';
 import { ThreeProvider } from './ThreeContext';
 import ThreeEnvironment from './ThreeEnvironment';
+import init from '@/three/init';
 
 export default function Client() {
-  const [scene, setScene] = useState<THREE.Scene | null>(null);
+    const [scene, setScene] = useState<THREE.Scene | null>(null);
 	const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>(null);
+	const ref = useRef<HTMLDivElement>(null);
   
 	useEffect(() => {
 		hydrate();
@@ -21,22 +23,22 @@ export default function Client() {
 			type: ToastType.INFO
 		});
 
-    // Now safe to use window
+    	// Now safe to use window
 		const newScene = new THREE.Scene();
 		const newCamera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 45, 30000);
 
-		setScene(newScene);
-		setCamera(newCamera);
-	}, []);
+		if (!scene) setScene(newScene);
+		if (!camera) setCamera(newCamera);
+		if (!ref.current) return;
+
+		init(newScene, newCamera, ref.current);
+	}, [scene, camera, ref]);
 
   if (!(scene && camera)) return null;
 
 	return (
 		<ThreeProvider scene={scene} camera={camera}>
-			<ThreeEnvironment>
-				<Terrarium />
-				<TerrariumTwo />
-			</ThreeEnvironment>
+			<div ref={ref} />
 		</ThreeProvider>
 	);
 }
