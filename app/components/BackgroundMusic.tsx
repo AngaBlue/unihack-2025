caed59b31b7e1b56df34a2e9a492a88358b01013
AngaBlue <contact@anga.blue>
2025-03-16 00:12:36 +1100

    feat: background music volume adjustments

--- app/components/BackgroundMusic.tsx
+++ app/components/BackgroundMusic.tsx
@@ -2,14 +2,23 @@
 import { useEffect } from 'react';
 import useSound from 'use-sound';
 
-const BASE_VOLUME = 0.5;
-const MUSIC_VOLUME = 1.5 * BASE_VOLUME;
-const BUBBLES_VOLUME = 0.5 * BASE_VOLUME;
+const db = (x: number) => 10 ** (x / 10)
+
+const BASE_VOLUME = 0.3;
+const OWL_VOLUME = BASE_VOLUME * db(-5);
+const MUSIC_VOLUME = BASE_VOLUME * db(1);
+const BIRDS_VOLUME = BASE_VOLUME * db(-13);
+const BUBBLES_VOLUME = BASE_VOLUME * db(-13);
 
 export default function BackgroundMusic() {
 	const [playMusic, { stop: stopMusic }] = useSound('/sounds/music.mp3', { loop: true, volume: MUSIC_VOLUME });
-	const [playOwl1] = useSound('/sounds/owl-1.mp3', { volume: BASE_VOLUME });
-	const [playOwl2] = useSound('/sounds/owl-2.mp3', { volume: BASE_VOLUME });
+	const [playOwl1] = useSound('/sounds/owl-1.mp3', { volume: OWL_VOLUME });
+	const [playOwl2] = useSound('/sounds/owl-2.mp3', { volume: OWL_VOLUME });
+	const [playBird1] = useSound('/sounds/bird_rain_1.mp3', { volume: BIRDS_VOLUME });
+	const [playBird2] = useSound('/sounds/bird_rain_2.mp3', { volume: BIRDS_VOLUME });
+	const [playBird3] = useSound('/sounds/bird_rain_3.mp3', { volume: BIRDS_VOLUME });
+	const [playBird4] = useSound('/sounds/bird_rain_4.mp3', { volume: BIRDS_VOLUME });
+	const [playBird5] = useSound('/sounds/bird_rain_5.mp3', { volume: BIRDS_VOLUME });
 	const [playBubbles] = useSound('/sounds/bubbles.mp3', { volume: BUBBLES_VOLUME });
 
 	// Background Music
@@ -42,12 +51,33 @@ export default function BackgroundMusic() {
 		};
 	}, [playOwl1, playOwl2]);
 
+	// Bird Rain
+	useEffect(() => {
+		let timeout: ReturnType<typeof setTimeout> | null = null;
+		const birdSounds = [playBird1, playBird2, playBird3, playBird4, playBird5];
+		const playRandomEffect = () => {
+			const MIN = 8_000;
+			const MAX = 18_000;
+			const randomTime = Math.random() * (MAX - MIN) + MIN;
+			timeout = setTimeout(() => {
+				const randomBird = birdSounds[Math.floor(Math.random() * birdSounds.length)];
+				randomBird();
+				playRandomEffect();
+			}, randomTime);
+		};
+
+		playRandomEffect();
+		return () => {
+			if (timeout) clearTimeout(timeout);
+		};
+	}, [playBird1, playBird2, playBird3, playBird4, playBird5]);
+
 	// Bubbles
 	useEffect(() => {
 		let timeout: ReturnType<typeof setTimeout> | null = null;
 		const playRandomEffect = () => {
-			const MIN = 10_000;
-			const MAX = 20_000;
+			const MIN = 20_000;
+			const MAX = 30_000;
 			const randomTime = Math.random() * (MAX - MIN) + MIN;
 			timeout = setTimeout(() => {
 				playBubbles();
