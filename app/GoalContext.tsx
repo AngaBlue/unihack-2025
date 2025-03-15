5674e9558915008fe4c3067e3cbb36801db2289d
AngaBlue <contact@anga.blue>
2025-03-16 00:31:30 +1100

    fix: context types

--- app/GoalContext.tsx
+++ app/GoalContext.tsx
@@ -1,13 +1,14 @@
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import type React from 'react';
+import { type ReactNode, createContext, useContext, useState } from 'react';
 
 // Define the type for context state
 interface GoalContextType {
-  name: string;
-  goal: string;
-  skillTree: string[];
-  setName: (name: string) => void;
-  setGoal: (goal: string) => void;
-  setSkillTree: (skillTree) => void;
+	name: string;
+	goal: string;
+	skillTree: string[];
+	setName: (name: string) => void;
+	setGoal: (goal: string) => void;
+	setSkillTree: (skillTree: string[]) => void;
 }
 
 // Create the context with an initial empty state
@@ -15,22 +16,18 @@ const GoalContext = createContext<GoalContextType | undefined>(undefined);
 
 // Create a provider component
 export const GoalProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [name, setName] = useState<string>(''); // Initialize with an empty string or any default value
-  const [goal, setGoal] = useState<string>(''); // Same for goal state
-  const [skillTree, setSkillTree] = useState(''); 
+	const [name, setName] = useState<string>(''); // Initialize with an empty string or any default value
+	const [goal, setGoal] = useState<string>(''); // Same for goal state
+	const [skillTree, setSkillTree] = useState<string[]>([]);
 
-  return (
-    <GoalContext.Provider value={{ name, goal, setName, setGoal, skillTree, setSkillTree}}>
-      {children}
-    </GoalContext.Provider>
-  );
+	return <GoalContext.Provider value={{ name, goal, setName, setGoal, skillTree, setSkillTree }}>{children}</GoalContext.Provider>;
 };
 
 // Custom hook to use the GoalContext
 export const useGoalContext = (): GoalContextType => {
-  const context = useContext(GoalContext);
-  if (!context) {
-    throw new Error('useGoalContext must be used within a GoalProvider');
-  }
-  return context;
+	const context = useContext(GoalContext);
+	if (!context) {
+		throw new Error('useGoalContext must be used within a GoalProvider');
+	}
+	return context;
 };
