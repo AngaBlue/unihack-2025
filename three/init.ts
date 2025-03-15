fa375393051fc4de5dfad279736438cc44c3326d
AngaBlue <contact@anga.blue>
2025-03-15 17:54:23 +1100

    chore: lint

--- three/init.ts
+++ three/init.ts
@@ -3,244 +3,187 @@ import { OrbitControls } from 'three-stdlib';
 import { MTLLoader } from 'three-stdlib';
 import { OBJLoader } from 'three-stdlib';
 import { DragControls } from 'three-stdlib';
-import { FBXLoader } from 'three-stdlib'; 
+import { FBXLoader } from 'three-stdlib';
+import { ImprovedNoise } from 'three-stdlib';
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
+import createSkybox from './createSkybox';
 import { getFresnelMat } from './getFresnelMat';
-import { ImprovedNoise } from 'three-stdlib'; 
 
-var startColor:any;
+var startColor: any;
 export default function init(scene: THREE.Scene, camera: THREE.PerspectiveCamera, div: HTMLDivElement) {
+	function getCorona() {
+		const radius = 0.9;
+		const material = new THREE.MeshBasicMaterial({
+			color: 0xffffff,
+			side: THREE.BackSide
+		});
+		const geo = new THREE.IcosahedronGeometry(radius, 6);
+		const mesh = new THREE.Mesh(geo, material);
+		const noise = new ImprovedNoise();
 
-    function getCorona() {
-        const radius = 0.9;
-        const material = new THREE.MeshBasicMaterial({
-            color: 0xffffff,
-            side: THREE.BackSide,
-        });
-        const geo = new THREE.IcosahedronGeometry(radius, 6);
-        const mesh = new THREE.Mesh(geo, material);
-        const noise = new ImprovedNoise();
-    
-        let v3 = new THREE.Vector3();
-        let p = new THREE.Vector3();
-        let pos = geo.attributes.position;
-        // @ts-ignore
-        pos.usage = THREE.DynamicDrawUsage;
-        const len = pos.count;
-    
-        function update(t: any) {
-            for (let i = 0; i < len; i += 1) {
-                p.fromBufferAttribute(pos, i).normalize();
-                v3.copy(p).multiplyScalar(3.0);
-                let ns = noise.noise(v3.x + Math.cos(t), v3.y + Math.sin(t), v3.z + t);
-                v3.copy(p)
-                    .setLength(radius)
-                    .addScaledVector(p, ns * 0.4);
-                pos.setXYZ(i, v3.x, v3.y, v3.z);
-            }
-            pos.needsUpdate = true;
-        }
-        mesh.userData.update = update;
-        return mesh;
-    }
+		const v3 = new THREE.Vector3();
+		const p = new THREE.Vector3();
+		const pos = geo.attributes.position;
+		// @ts-ignore
+		pos.usage = THREE.DynamicDrawUsage;
+		const len = pos.count;
 
-    camera.position.set(0, 0, 500);
+		function update(t: number) {
+			for (let i = 0; i < len; i += 1) {
+				p.fromBufferAttribute(pos, i).normalize();
+				v3.copy(p).multiplyScalar(3.0);
+				const ns = noise.noise(v3.x + Math.cos(t), v3.y + Math.sin(t), v3.z + t);
+				v3.copy(p)
+					.setLength(radius)
+					.addScaledVector(p, ns * 0.4);
+				pos.setXYZ(i, v3.x, v3.y, v3.z);
+			}
+			pos.needsUpdate = true;
+		}
+		mesh.userData.update = update;
+		return mesh;
+	}
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    div.appendChild(renderer.domElement);
-    
-    // adding mesh for sphere planet
-    
-    const geometry = new THREE.IcosahedronGeometry(1, 6);
-    const material = new THREE.MeshStandardMaterial({
-        emissive: 0x5D3FD3,
-    });
-    const sphere = new THREE.Mesh( geometry, material); 
-    sphere.castShadow = true;
+	camera.position.set(0, 0, 500);
 
-    const sunRimMat = getFresnelMat({ rimHex: 0xffffff, facingHex: 0x000000 });
-    const rimMesh = new THREE.Mesh(geometry, sunRimMat);
-    sphere.scale.setScalar(50);
-    rimMesh.scale.setScalar(1.01);
-    sphere.add(rimMesh);
+	const renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	div.appendChild(renderer.domElement);
 
-    const coronaMesh = getCorona();
-    sphere.add(coronaMesh);
+	// adding mesh for sphere planet
 
-    sphere.userData.update = (t: number) => {
-        // sphere.rotation.y = t;
-        coronaMesh.userData.update(t);
-    };
+	const geometry = new THREE.IcosahedronGeometry(1, 6);
+	const material = new THREE.MeshStandardMaterial({
+		emissive: 0x5d3fd3
+	});
+	const sphere = new THREE.Mesh(geometry, material);
+	sphere.castShadow = true;
 
+	const sunRimMat = getFresnelMat({ rimHex: 0xffffff, facingHex: 0x000000 });
+	const rimMesh = new THREE.Mesh(geometry, sunRimMat);
+	sphere.scale.setScalar(50);
+	rimMesh.scale.setScalar(1.01);
+	sphere.add(rimMesh);
 
+	const coronaMesh = getCorona();
+	sphere.add(coronaMesh);
 
-    /**
-     * 
-     *  const sunMat = new THREE.MeshStandardMaterial({
-        emissive: 0xff0000,
-    });
-    const geo = new THREE.IcosahedronGeometry(1, 6);
-    const sun = new THREE.Mesh(geo, sunMat);
+	sphere.userData.update = (t: number) => {
+		// sphere.rotation.y = t;
+		coronaMesh.userData.update(t);
+	};
 
-    const sunRimMat = getFresnelMat({ rimHex: 0xffff99, facingHex: 0x000000 });
-    const rimMesh = new THREE.Mesh(geo, sunRimMat);
-    rimMesh.scale.setScalar(1.01);
-    sun.add(rimMesh);
+	/**
+	 * SPHERE POSITION IS 10,10,10
+	 */
+	sphere.position.set(0, 0, 0);
 
-    const coronaMesh = getCorona();
-    sun.add(coronaMesh);
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.target = sphere.position;
+	controls.addEventListener('change', () => renderer.render(scene, camera));
 
-    const sunLight = new THREE.PointLight(0xffff99, 10);
-    sun.add(sunLight);
-    sun.userData.update = (t) => {
-        sun.rotation.y = t;
-        coronaMesh.userData.update(t);
-    };
-     */
+	/* Bug fix, ensure that users cannot exceed skybox dimension */
+	controls.minDistance = sphere.position.getComponent(1) + 300;
+	controls.maxDistance = 1500;
+	scene.add(createSkybox());
 
-    /**
-     * SPHERE POSITION IS 10,10,10
-     */
-    sphere.position.set(0,0,0);
+	const directionalLight = new THREE.DirectionalLight(0xe8b5bd, 0.8);
+	directionalLight.position.set(100, 100, 100).normalize();
+	directionalLight.target = sphere;
+	scene.add(directionalLight);
 
-        
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target = sphere.position;
-    controls.addEventListener('change', () => renderer.render(scene, camera));
+	const ambientLight = new THREE.AmbientLight(0xe8b5bd, 0.00005);
+	ambientLight.position.set(-100, 100, -100);
 
-    /* Bug fix, ensure that users cannot exceed skybox dimension */
-    controls.minDistance = sphere.position.getComponent(1) + 300;
-    controls.maxDistance = 1500;
+	scene.add(ambientLight);
 
-    const texture_ft = new THREE.TextureLoader().load('/space_ft.png');
-    const texture_bk = new THREE.TextureLoader().load('/space_bk.png');
-    const texture_up = new THREE.TextureLoader().load('/space_up.png');
-    const texture_dn = new THREE.TextureLoader().load('/space_dn.png');
-    const texture_rt = new THREE.TextureLoader().load('/space_rt.png');
-    const texture_lf = new THREE.TextureLoader().load('/space_lf.png');
+	const animationActions: THREE.AnimationAction[] = [];
+	let activeAction: THREE.AnimationAction;
+	let mixer: THREE.AnimationMixer | null = null;
+	/**
+	 *
+	 * ADDING ROBOT THROUGH FBX LOADER
+	 */
 
-    // texture array
-    const skyboxArray = [
-        new THREE.MeshBasicMaterial({ map: texture_ft, side: THREE.BackSide }),
-        new THREE.MeshBasicMaterial({ map: texture_bk, side: THREE.BackSide }),
-        new THREE.MeshBasicMaterial({ map: texture_up, side: THREE.BackSide }),
-        new THREE.MeshBasicMaterial({ map: texture_dn, side: THREE.BackSide }),
-        new THREE.MeshBasicMaterial({ map: texture_rt, side: THREE.BackSide }),
-        new THREE.MeshBasicMaterial({ map: texture_lf, side: THREE.BackSide })
-    ];
+	const robotGroup = new THREE.Group(); // Create a parent group
+	robotGroup.position.set(100, 10, 100); // Fix this position in the world
+	scene.add(robotGroup); // Add to scene
 
-    const skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
-    const skybox = new THREE.Mesh(skyboxGeo, skyboxArray);
-    scene.add(skybox);
+	const fbxLoader = new FBXLoader();
+	fbxLoader.load('Robot.fbx', object => {
+		object.scale.set(50, 50, 50); // Scale as needed
+		robotGroup.add(object); // Add robot to parent group instead of directly to the scene
 
-    const directionalLight = new THREE.DirectionalLight(0xE8B5BD, 0.8);
-    directionalLight.position.set(100, 100, 100).normalize();
-    directionalLight.target = sphere;
-    scene.add(directionalLight);
+		mixer = new THREE.AnimationMixer(object);
+		object.animations.forEach(clip => {
+			const action = mixer!.clipAction(clip);
+			animationActions.push(action);
+		});
 
-    const ambientLight = new THREE.AmbientLight(0xE8B5BD, 0.00005);
-    ambientLight.position.set(-100, 100, -100);
+		if (animationActions.length > 0) {
+			activeAction = animationActions[0];
+			activeAction.play();
+		}
+	});
 
-    scene.add(ambientLight);
+	/**
+	 * adding mushroom
+	 */
+	var objects: THREE.Group<THREE.Object3DEventMap>[] = [];
 
+	const mtlLoader = new MTLLoader();
+	mtlLoader.setPath('/');
+	mtlLoader.load('mushrooms.mtl', materials => {
+		materials.preload();
+		const objLoader = new OBJLoader();
+		objLoader.setMaterials(materials);
+		objLoader.setPath('/');
+		objLoader.load('mushrooms.obj', object => {
+			object.position.set(50, 50, 50);
+			object.scale.set(10, 10, 10);
+			scene.add(object);
+			objects.push(object);
+		});
+	});
+	/**
+	 * Adding drag movement to mushroom
+	 */
 
-    let animationActions: THREE.AnimationAction[] = [];
-    let activeAction: THREE.AnimationAction;
-    let mixer: THREE.AnimationMixer | null = null;
-    /** 
-     * 
-     * ADDING ROBOT THROUGH FBX LOADER
-     */
+	const dragControls = new DragControls(objects, camera, renderer.domElement);
+	dragControls.addEventListener('dragstart', dragStartCallback);
+	dragControls.addEventListener('dragend', dragendCallback);
 
-    const robotGroup = new THREE.Group(); // Create a parent group
-    robotGroup.position.set(100, 10, 100); // Fix this position in the world
-    scene.add(robotGroup); // Add to scene
-    
-    const fbxLoader = new FBXLoader();
-    fbxLoader.load('Robot.fbx', (object) => {
-        object.scale.set(50, 50, 50); // Scale as needed
-        robotGroup.add(object); // Add robot to parent group instead of directly to the scene
-    
-        mixer = new THREE.AnimationMixer(object);
-        object.animations.forEach((clip) => {
-            const action = mixer!.clipAction(clip);
-            animationActions.push(action);
-        });
-    
-        if (animationActions.length > 0) {
-            activeAction = animationActions[0];
-            activeAction.play();
-        }
-    });
+	const pointLockControls = new PointerLockControls(camera, document.body);
+	/**
+	 * Cast shadow (darken colour) of selected mushroom
+	 */
+	function dragStartCallback(event: any) {
+		// pointLockControls.lock()
+		controls.enabled = false; // Disable OrbitControls
 
+		startColor = event.object.material.color.getHex();
+		event.object.material.color.setHex(0x000000);
+	}
 
+	function dragendCallback(event: any) {
+		controls.enabled = true;
+		event.object.material.color.setHex(startColor);
+		// pointLockControls.unlock()
+	}
 
-    /**
-     * adding mushroom
-     */
-    var objects: THREE.Group<THREE.Object3DEventMap>[] = [];
+	const clock = new THREE.Clock();
 
+	// ANIMATION LOOP
+	const animate = () => {
+		requestAnimationFrame(animate);
 
-    const mtlLoader = new MTLLoader();
-    mtlLoader.setPath('/');
-    mtlLoader.load('mushrooms.mtl', materials => {
-        materials.preload();
-        const objLoader = new OBJLoader();
-        objLoader.setMaterials(materials);
-        objLoader.setPath('/');
-        objLoader.load('mushrooms.obj', object => {
-            object.position.set(50, 50, 50);
-            object.scale.set(10, 10, 10);
-            scene.add(object);
-            objects.push(object)
-        });
-    }
-    )
-    /**
-     * Adding drag movement to mushroom
-     */
-    
-	let dragControls = new DragControls( objects, camera, renderer.domElement );
-	dragControls.addEventListener( 'dragstart', dragStartCallback );
-	dragControls.addEventListener( 'dragend', dragendCallback );
-    
-    const pointLockControls = new PointerLockControls( camera, document.body );
-    /** 
-     * Cast shadow (darken colour) of selected mushroom 
-     */ 
-    function dragStartCallback(event: any) {
-        // pointLockControls.lock()
-        controls.enabled = false; // Disable OrbitControls
+		const delta = clock.getDelta(); // Get time elapsed since last frame
+		if (mixer) mixer.update(delta); // Update animation
+		sphere.userData.update(clock.elapsedTime);
 
+		renderer.render(scene, camera);
+	};
 
-        startColor = event.object.material.color.getHex();
-        event.object.material.color.setHex(0x000000);
-    }
-
-    function dragendCallback(event: any) {
-
-        controls.enabled = true; 
-        event.object.material.color.setHex(startColor);
-        // pointLockControls.unlock()
-
-    }
-
-    const clock = new THREE.Clock(); 
-
-    // ANIMATION LOOP
-    const animate = () => {
-        requestAnimationFrame(animate);
-    
-        const delta = clock.getDelta(); // Get time elapsed since last frame
-        if (mixer) mixer.update(delta); // Update animation
-        sphere.userData.update(clock.elapsedTime)
-    
-        renderer.render(scene, camera);
-    };
-
-    animate();
-    scene.add( sphere );
-
-
+	animate();
+	scene.add(sphere);
 }
